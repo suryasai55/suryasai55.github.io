89b6ee6b37bdbb27d6da5b4e229770c2c6fa0ef9
Morampudi Surya Sai <suryasaiofficial@gmail.com>
2026-01-10 11:33:41 +0530

    Fix case sensitivity in Contact model import

--- back_end/routes/contact.js
+++ back_end/routes/contact.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Contact = require("../models/Contact");
+const Contact = require("../models/contact");
 const nodemailer = require("nodemailer");
 
 const router = express.Router();
